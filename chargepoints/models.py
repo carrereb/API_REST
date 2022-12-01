162a618b27fd6d432155417fbbd998a48348d2fc
carrereb <bruno.carrere11@gmail.com>
2022-12-01 09:27:54 +0100

    models chargepoint and customer

--- chargepoints/models.py
+++ chargepoints/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Chargepoint(models.Model):
+    name = models.fields.CharField(max_length=100)
+    number_of_chargepoint = models.fields.IntegerField()
+    puissance_max_w = models.fields.FloatField()
+
+class Customer(models.Model):
+    name = models.fields.CharField(max_length=100)
+    # chargepoint = ...
+    total_payed = models.fields.FloatField()
