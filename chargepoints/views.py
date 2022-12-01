5bb5a4a1319082a357eabe1b1f0bf4c6b94d2d0c
carrereb <bruno.carrere11@gmail.com>
2022-12-01 09:27:32 +0100

    add view customers and chargepoint

--- chargepoints/views.py
+++ chargepoints/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def chargepoint(request):
+    return HttpResponse('<h1>Chargepoint</h1>')
+
+def customers(request):
+    return HttpResponse('<h1>Customers</h1>')
