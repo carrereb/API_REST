8c98ffb6750f520279335c9907a86004762ec8ad
carrereb <bruno.carrere11@gmail.com>
2022-12-01 09:35:58 +0100

    update display views

--- chargepoints/views.py
+++ chargepoints/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from chargepoints.models import Chargepoint, Customer
+from django.shortcuts import render
 
 def chargepoint(request):
-    return HttpResponse('<h1>Chargepoint</h1>')
+    chargepoints = Chargepoint.ojects.all()
+    return render(request, 'chargepoints/chargepoints.json')
 
 def customers(request):
+    customers = Customer.ojects.all()
     return HttpResponse('<h1>Customers</h1>')
